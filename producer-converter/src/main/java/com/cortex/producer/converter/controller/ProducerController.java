be035d2e4f7c4a47ea601e786270e066111b7a8c
Lucas <lwilker24@gmail.com>
2022-08-07 00:12:45 -0300

    producer: criando comunicação controler e service

--- producer-converter/src/main/java/com/cortex/producer/converter/controller/ProducerController.java
+++ producer-converter/src/main/java/com/cortex/producer/converter/controller/ProducerController.java
@@ -1,11 +1,37 @@
 package com.cortex.producer.converter.controller;
 
+import com.cortex.producer.converter.service.ProducerService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dto.ConversionDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController("CurrencyConverterController")
 @RequestMapping(value = "cotacaomoedacortex")
 public class ProducerController {
+
+    private ProducerService producerService;
+
+    public ProducerController(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+
+    @GetMapping("/conversao")
+    public ResponseEntity<String> currencyConversion(@RequestParam @Valid String dataCotacao,
+                                                            @RequestParam @Valid String moedaOrigem,
+                                                            @RequestParam @Valid String moedaFinal,
+                                                            @RequestParam @Valid Double valorDesejado) throws JsonProcessingException {
+
+        var conversionDTO = this.producerService.sendMessageConversion(dataCotacao, moedaOrigem, moedaFinal, valorDesejado);
+        return ResponseEntity.status(HttpStatus.OK).body(conversionDTO);
+    }
 }
--- producer-converter/src/main/java/com/cortex/producer/converter/service/ProducerService.java
+++ producer-converter/src/main/java/com/cortex/producer/converter/service/ProducerService.java
@@ -1,9 +1,18 @@
 package com.cortex.producer.converter.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import consts.RabbitMQConsts;
+import dto.ConversionDTO;
+import dto.RequestDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ProducerService {
+
+    public String sendMessageConversion(String dataCotacao, String moedaOrigem, String moedaFinal, Double valorDesejado) throws JsonProcessingException {
+        return "teste";
+    }
 }
